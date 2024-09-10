5252d290e19eeafae78273c09bc8bb953f223279
rmishgoog <rohitmishra@google.com>
2024-09-09 22:15:22 -0500

    error handling middleware

--- foundations/web/middleware.go
+++ foundations/web/middleware.go
@@ -8,6 +8,10 @@ func wrapMiddleware(mw []MidHandler, hanlder Handler) Handler {
 	for i := len(mw) - 1; i >= 0; i-- {
 		mwFunc := mw[i]
 		if mwFunc != nil {
+			// First iteration - handler = liveness or readiness, middleware = error handling (m())
+			// h() will be the handler which will embed the liveness or readiness call w/ error handling.
+			// second iteration - handler = handler will be the handler which will embed the liveness or readiness call w/ error handling.
+			// h() returned will be logger middleware wrapping around it.
 			hanlder = mwFunc(hanlder)
 		}
 	}
